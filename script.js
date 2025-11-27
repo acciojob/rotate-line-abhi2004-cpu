be24917b2a29282d47f17e732e68e49edec98837
Abhishek Gorakhnath Deokar <abhishekdeokar20@gmail.com>
2025-11-27 19:01:03 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-let angle = 0;
-const line = document.getElementById("line");
-
-setInterval(() => {
-    angle += 2;
-    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-}, 20);
-
